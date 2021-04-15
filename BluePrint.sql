2d4bb19041939a3600d38e00bf68b10ecf10921e
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2021-04-15 18:55:25 -0300

    Reforma BluePrint

--- BluePrint.sql
+++ BluePrint.sql
@@ -1,46 +1,30 @@
 create database BluePrint
 go
-use BluePrint
+use Blueprint
 go
-create table TipoCliente(
-	ID int not null primary key identity (1,1),
-	Tipo char(50) not null
-)
-go
-create table Cliente(
-	ID int not null primary key identity(1,1),
-	RazonSocial char(50) not null,
-	cuit int not null unique,
-	IDTipoCliente int not null foreign key references TipoCliente(ID)
+create Table TipoClientes(
+	ID int primary key identity(1,1) not null,
+	Tipo varchar(50) not null
 )
 go
-create table Contacto(
-	ID int not null primary key foreign key references Cliente(ID),
-	Email char(50) null unique,
-	Telefono char(10) null unique,
-	Celular char(10) null unique
+create table Clientes(
+	ID int primary key identity(1,1) not null,
+	RazonSocial varchar(50) not null,
+	cuit varchar(11) unique not null,
+	IDtipoCliente int foreign key references TipoClientes(ID) not null,
+	Email varchar(50) null,
+	Telefono varchar(13) null,
+	Celular varchar(13) null,
 )
 go
-create table Proyecto(
-	ID int not null primary key identity(1,1),
-	Nombre char(50) not null,
-	IDCliente int not null foreign key references Cliente(ID),
-	Estado bit not null
+create table Proyectos(
+	ID int primary key identity(1,1) not null,
+	IDCliente int foreign key references Clientes(ID) not null,
+	Nombre varchar(50) not null,
+	Descripcion varchar(512) null,
+	FechaInicio date not null,
+	FechaFin date null,
+	Costo money check(Costo>0) not null,
+	Estado bit not null,
+	check (FechaInicio<FechaFin)
 )
-create table Descripcion(
-	ID int not null primary key foreign key references Proyecto(ID),
-	Descripcion char(200) not null
-)
-go
-create table Costo(
-	ID int not null primary key foreign key references Proyecto(ID),
-	Costo money not null
-)
-go
-create table Fecha(
-	ID int not null primary key foreign key references Proyecto(ID),
-	Inicio date not null,
-	Fin date null
-)
-
-
